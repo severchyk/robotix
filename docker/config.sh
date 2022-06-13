70c1a610de1066b976b0d3846a4156cd1991a633
Severyn Matsiak <severus008@gmail.com>
2022-06-14 02:39:43 +0300

    removed timezone setup

--- docker/config.sh
+++ docker/config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo -e "\n**** Configuring timezone ****\n"
-echo 'Europe/Kiev' | sudo tee /etc/timezone
-dpkg-reconfigure --frontend noninteractive tzdata
-
 echo -e "\n**** Pulling image from repo ****\n"
 docker pull severchyk/robotix:focal
 
